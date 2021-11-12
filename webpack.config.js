d36fd9571bf35260db5e23d9e42823642106c377
Lee Fowlow <lee_fowlow@hotmail.com>
2021-11-12 16:15:05 -0330

    webpack partly set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,12 @@
-var debug = process.env.NODE_ENV !== "production";
-const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-    context: __dirname,
-    devtool: debug ? "inline-sourcemap" : null,
-    entry: "./index.js",
-    resolve: {
-      extensions: ['.js']
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, 'index.js')
     },
-    // module: {
-    // rules: [{
-    //     test: /\.js$/,
-    //     use: 'babel-loader'
-    //   }]
-    // },
     output: {
-      path: __dirname + "public/javascripts",
-    //   filename: "scripts.min.js"
-    },
-    plugins: debug ? [] : [
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    ],
-  };
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    }
+}
